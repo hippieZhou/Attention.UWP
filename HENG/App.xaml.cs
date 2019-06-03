579f12c769936c0b983808510ea8fd8661adca48
hippieZhou <hippiezhou@outlook.com>
2019-06-03 14:50:25 +0800

    updtae

--- HENG/App.xaml.cs
+++ HENG/App.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using GalaSoft.MvvmLight.Threading;
 using HENG.Services;
 using System;
-using System.Diagnostics;
 using System.Threading;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
@@ -14,31 +13,33 @@ using Windows.UI.Xaml.Navigation;
 
 namespace HENG
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
     sealed partial class App : Application
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
         public App()
         {
             this.InitializeComponent();
             this.Suspending += OnSuspending;
-            this.UnhandledException += OnUnhandledException;
+            UnhandledException += App_UnhandledException;
         }
 
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
-        /// </summary>
-        /// <param name="e">Details about the launch request and process.</param>
+        private void App_UnhandledException(object sender, Windows.UI.Xaml.UnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+        }
+
         protected override async void OnLaunched(LaunchActivatedEventArgs e)
         {
-            ExtendAcrylicIntoTitleBar();
+#if DEBUG
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                DebugSettings.EnableFrameRateCounter = true;
+            }
+#endif
 
+            if (e.PrelaunchActivated)
+                return;
+
+            ExtendAcrylicIntoTitleBar();
             void ExtendAcrylicIntoTitleBar()
             {
                 CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
@@ -48,58 +49,16 @@ namespace HENG
                 titleBar.ButtonForegroundColor = (Color)Resources["SystemBaseHighColor"];
             }
 
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            if (!(Window.Current.Content is Frame rootFrame))
-            {
-                // Create a Frame to act as the navigation context and navigate to the first page
-                rootFrame = new Frame();
-
-                rootFrame.NavigationFailed += OnNavigationFailed;
-
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    //TODO: Load state from previously suspended application
-                }
-
-                // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
-            }
-
-            if (e.PrelaunchActivated == false)
-            {
-                if (rootFrame.Content == null)
-                {
-                    // When the navigation stack isn't restored navigate to the first page,
-                    // configuring the new page by passing required information as a navigation
-                    // parameter
-                    rootFrame.Navigate(typeof(Shell), e.Arguments);
-                }
-                // Ensure the current window is active
-                Window.Current.Activate();
-            }
-
+            CreateFrameWithArgurments(e.Arguments);
             DispatcherHelper.Initialize();
             await DownloadService.AttachToDownloadsAsync(new CancellationTokenSource());
         }
 
-        /// <summary>
-        /// Invoked when Navigation to a certain page fails
-        /// </summary>
-        /// <param name="sender">The Frame which failed navigation</param>
-        /// <param name="e">Details about the navigation failure</param>
         void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
         {
             throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
         }
 
-        /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
-        /// </summary>
-        /// <param name="sender">The source of the suspend request.</param>
-        /// <param name="e">Details about the suspend request.</param>
         private void OnSuspending(object sender, SuspendingEventArgs e)
         {
             var deferral = e.SuspendingOperation.GetDeferral();
@@ -107,21 +66,40 @@ namespace HENG
             deferral.Complete();
         }
 
-        private void OnUnhandledException(object sender, Windows.UI.Xaml.UnhandledExceptionEventArgs e)
+        protected override void OnBackgroundActivated(BackgroundActivatedEventArgs args)
         {
-            Trace.WriteLine(e.Exception);
+            var taskInstance = args.TaskInstance;
+
+            taskInstance.Canceled += (sender, reason) => 
+            {
+                var deferral = taskInstance.GetDeferral();
+                deferral.Complete();
+            };
+            base.OnBackgroundActivated(args);
         }
 
-        protected override void OnActivated(IActivatedEventArgs args)
+        protected override void OnActivated(IActivatedEventArgs e)
         {
-            switch (args.Kind)
+            string arg = null;
+            if (e is ToastNotificationActivatedEventArgs)
             {
-                case ActivationKind.ToastNotification:
-                    break;
-                default:
-                    break;
+                var toastActivationArgs = e as ToastNotificationActivatedEventArgs;
+                arg = toastActivationArgs.Argument;
             }
-            base.OnActivated(args);
+            CreateFrameWithArgurments(arg);
+        }
+
+        private Frame CreateFrameWithArgurments(string args)
+        {
+            if (!(Window.Current.Content is Frame rootFrame))
+            {
+                rootFrame = new Frame();
+                rootFrame.NavigationFailed += OnNavigationFailed;
+                Window.Current.Content = rootFrame;
+            }
+            rootFrame.Navigate(typeof(Shell), args);
+            Window.Current.Activate();
+            return rootFrame;
         }
     }
 }
