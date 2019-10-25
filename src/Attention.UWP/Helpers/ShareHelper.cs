56c9aea5eb5a516e1de033442bc3c4f68cb512d5
hippieZhou <hippiezhou@outlook.com>
2019-10-25 23:28:44 +0800

    update

--- src/Attention.UWP/Helpers/ShareHelper.cs
+++ src/Attention.UWP/Helpers/ShareHelper.cs
@@ -1,7 +1,9 @@
 ﻿using Attention.UWP.Extensions;
 using Attention.UWP.Models;
+using Microsoft.Toolkit.Uwp.UI;
 using PixabaySharp.Models;
 using System;
+using System.Diagnostics;
 using Windows.ApplicationModel.DataTransfer;
 using Windows.Storage.Streams;
 
@@ -14,18 +16,26 @@ namespace Attention.UWP.Helpers
             DataTransferManager dataTransferManager = DataTransferManager.GetForCurrentView();
             dataTransferManager.DataRequested += (manager, args) =>
             {
+                var deferral = args.Request.GetDeferral();
+
                 args.Request.Data.ShareCompleted += (sender, e) =>
                 {
-
+                    deferral.Complete();
+                    Debug.WriteLine("done");
                 };
 
                 ShareSourceData config = new ShareSourceData($"{App.Settings.Name}", "AppDescription".GetLocalized());
-                config.SetApplicationLink(new Uri(item.WebformatURL));
-                config.SetWebLink(new Uri(item.WebformatURL));
+                config.SetWebLink(new Uri(item.FullHDImageURL ?? item.ImageURL));
+                config.SetText($"User:{item.User};Type:{item.Type};Tags:{item.Tags}");
+                config.SetDeferredContent(StandardDataFormats.Bitmap, async () =>
+                {
+                    var cacheImage = await ImageCache.Instance.GetFileFromCacheAsync(new Uri(item.LargeImageURL));
+                    RandomAccessStreamReference imageStream = RandomAccessStreamReference.CreateFromFile(cacheImage);
+                    return imageStream;
+                });
                 args.Request.SetData(config);
-                args.Request.Data.SetBitmap(RandomAccessStreamReference.CreateFromUri(new Uri(item.PreviewURL)));
-                args.Request.Data.SetWebLink(new Uri(item.WebformatURL));
-                args.Request.FailWithDisplayText("Oops!");
+               
+                //args.Request.FailWithDisplayText("Oops!");
             };
 
             dataTransferManager.TargetApplicationChosen += (sender, e) =>
