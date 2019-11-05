8f16e8b991c363b0f937beb1c4613d61ceff9e06
hippieZhou <hippiezhou@outlook.com>
2019-11-05 12:02:25 +0800

    update

---
+++ src/Attention.UWP/Models/API_KEY.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Attention.UWP.Models
+{
+    /// <summary>
+    /// https://pixabay.com/api/docs/
+    /// </summary>
+    [JsonObject("API_KEY")]
+    public class API_KEY
+    {
+        [JsonProperty("Debug")]
+        public string Debug { get; set; }
+        [JsonProperty("Release")]
+        public string Release { get; set; }
+    }
+}
