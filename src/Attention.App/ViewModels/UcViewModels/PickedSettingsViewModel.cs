c68b461f270927fc7f47f78f62754c7d03e24fc3
hippieZhou <hippiezhou@outlook.com>
2020-05-11 15:18:01 +0800

    update

--- src/Attention.App/ViewModels/UcViewModels/PickedSettingsViewModel.cs
+++ src/Attention.App/ViewModels/UcViewModels/PickedSettingsViewModel.cs
@@ -25,13 +25,6 @@ namespace Attention.App.ViewModels.UcViewModels
             _eventAggregator = eventAggregator ?? throw new ArgumentNullException(nameof(eventAggregator));
         }
 
-        private string _dependencies;
-        public string Dependencies
-        {
-            get { return _dependencies; }
-            set { SetProperty(ref _dependencies, value); }
-        }
-
         private ICommand _loadCommand;
         public ICommand LoadCommand
         {
@@ -41,30 +34,6 @@ namespace Attention.App.ViewModels.UcViewModels
                 {
                     _loadCommand = new DelegateCommand(() =>
                     {
-                        Dependencies = @"
-## Toolkit
-
-- [Windows UI Library](https://github.com/microsoft/microsoft-ui-xaml)
-- [WindowsCommunityToolkit](https://github.com/windows-toolkit/WindowsCommunityToolkit)
-- [WindowsCompositionSamples](https://github.com/microsoft/WindowsCompositionSamples)
-- [Lottie-Windows](https://github.com/windows-toolkit/Lottie-Windows)
-- [Microsoft.Xaml.Behaviors.Uwp.Managed](https://github.com/Microsoft/XamlBehaviors)
-- [Prism.Unity(6.3.0)](https://github.com/PrismLibrary/Prism)
-- [AutoMapper](https://github.com/AutoMapper/AutoMapper)
-- [Newtonsoft.Json](https://github.com/JamesNK/Newtonsoft.Json)
-- [unsplasharp](https://github.com/rootasjey/unsplasharp)
-- [Serilog](https://github.com/serilog/serilog)
-
-## Helpers
-
-- [JuniperPhoton](https://github.com/JuniperPhoton)
-- [JustinXinLiu](https://github.com/JustinXinLiu)
-- [Niels Laute](https://github.com/niels9001)
-- [DinoChan](https://github.com/DinoChan)
-- [hhchaos](https://github.com/HHChaos)
-- [cnbluefire](https://github.com/cnbluefire)
-- [h82258652](https://github.com/h82258652)
-";
                     });
                 }
                 return _loadCommand;
